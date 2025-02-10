e16c2ab712255c58d882a3cfbdf7204b1e3b7d5f
AVVoy <Aleksandr.V.Voynov@yandex.ru>
2025-02-10 20:10:52 +0300

    Create app

---
+++ src/main/java/com/example/project_module5/ProjectModule5Application.java
@@ -0,0 +1,15 @@
+package com.example.project_module5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication(exclude = LiquibaseAutoConfiguration.class)
+public class ProjectModule5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectModule5Application.class, args);
+    }
+
+}
